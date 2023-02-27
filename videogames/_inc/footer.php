e81bc8080169bb12116b22c4c7094fdb56980455
vuthlang <103568613+vuthlang@users.noreply.github.com>
2023-02-27 18:28:01 +0100

    Exo : découper une page Web

---
+++ videogames/_inc/footer.php
@@ -0,0 +1,4 @@
+<div class="footer">&copy;
+    <span id="year">2023</span>
+    <span> H3 Hitema. All rights reserved.</span>
+</div>
---
+++ videogames/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once '_inc/header.php';
+require_once '_inc/nav.php';
+?>
+
+<main>
+    <div class="title">
+        <h1>Accueil</h1>
+    </div>
+</main>
+
+<?php
+require_once '_inc/footer.php';
+?>
